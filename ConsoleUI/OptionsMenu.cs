bf1ac6941c5f1bb7f6b9c2e264ad4ce830bfe891
GothicLolita229 <90853197+GothicLolita229@users.noreply.github.com>
2022-09-24 21:56:26 -0400

    Add files via upload

---
+++ ConsoleUI/OptionsMenu.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using MortuusClassLibrary;
+
+namespace ConsoleUI
+{
+    class OptionsMenu
+    {
+        
+        public static void ExploreMenu(char userChoice)
+        {
+
+            switch (userChoice)
+            {
+                case '1':
+                    Room.RoomDisplay(); 
+                    //Console.WriteLine(MakeLists.playerRooms);
+                    break;
+                case '2':
+                    Console.WriteLine(Weapon.weapons);
+                    break;
+                case '3':
+                    Console.WriteLine(Potion.potions);
+                    break;
+                case '4':
+                    Console.WriteLine(Treasure.treasures);
+                    break;
+                case '5':
+                    Item.ItemDisplay();
+                    Console.WriteLine(Item.items);
+                    break;
+                case '6':
+                    Console.WriteLine(Mob.mobs);
+                    break;
+                case '7':
+                    GameMenuSwitcher.MainMenu();
+                    break;
+                default:
+                    Exit();
+                    break;
+            }
+        }
+
+        public static void WriteExploreMenu()
+        {
+            Console.WriteLine("1. View Rooms");
+            Console.WriteLine("2. View Weapons");
+            Console.WriteLine("3. View Potions");
+            Console.WriteLine("4. View Treasures");
+            Console.WriteLine("5. View Items");
+            Console.WriteLine("6. View Mobs");
+            Console.WriteLine("7. Back to Main");
+        }
+
+        public static void Exit()
+        {
+            Environment.Exit(0);
+        }
+
+        public static void HelpMenu()
+        {
+            Console.WriteLine("1. Move North");
+            Console.WriteLine("2. Move South");
+            Console.WriteLine("3. Move East");
+            Console.WriteLine("4. Move West");
+            Console.WriteLine("5. Attack");
+            Console.WriteLine("6. Exit");
+            Console.WriteLine("7. Menu");
+        }
+
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using MortuusClassLibrary;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Room.RoomDisplay();
+            //Room.RoomExitDisplay(); //Change Exits to ints that display roomID numbers and then use those
+                                    // with a switch statement to move to different rooms. For example 
+                                    // The Foyer has exits (0, 1, 3, 5) where 0 is north and there is no room
+                                    // 1 is back to the entrance, 3 is the Great Hall and, 5 is store room.
+                                    // Or make a map with only two exits per room like Mr. Norris suggested
+            Game();
+        }
+
+         static void Game()
+        {
+            //string charName = LoadPlayer.PlayerInfo();
+            GameMenuSwitcher.MainMenu();
+        }
+
+    }
+}
